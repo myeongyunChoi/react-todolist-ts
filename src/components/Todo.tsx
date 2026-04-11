b19deef78abba2328473d2d80ec785cd9beec7a9
myeongyunChoi <104118233+myeongyunChoi@users.noreply.github.com>
2026-04-11 20:23:22 +0900

    feat: 할일 등록 기능 추가

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
 import TodoEditor from "./TodoEditor";
 import TodoList from "./TodoList";
 import TotoHeader from "./TotoHeader";
 
 export default function Todo() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const addTodo = (text: string) => {
+    setTodos((todos) => [
+      ...todos,
+      {
+        id: Date.now(),
+        text,
+        completed: false,
+      },
+    ]);
+  };
   return (
     <>
       <div className="todo">
         <TotoHeader />
         {/* <!-- 할 일 등록 --> */}
-        <TodoEditor />
+        <TodoEditor addTodo={addTodo} />
         {/* <!-- 할 일 목록 --> */}
         <TodoList />
       </div>
--- src/components/TodoEditor.tsx
+++ src/components/TodoEditor.tsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import Input from "./html/Input";
 
-export default function TodoEditor() {
+export default function TodoEditor({
+  addTodo,
+}: {
+  addTodo: (text: string) => void;
+}) {
+  const [text, setText] = useState("");
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    addTodo(text);
+    setText("");
+  };
   return (
     <>
-      <form className="todo__form">
+      <form className="todo__form" onSubmit={handleSubmit}>
         <div className="todo__editor">
           <Input
             type="text"
             className="todo__input"
             placeholder="Enter Todo List"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
           />
           <button className="todo__button" type="submit">
             Add
